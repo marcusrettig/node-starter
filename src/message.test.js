13ce35e9d542d53f7701131f74188e907fa5ca8b
Marcus Rettig <marcus0706@live.se>
2019-01-31 21:44:14 +0100

    Fix jest debug error caused by ! in test name

--- src/message.test.js
+++ src/message.test.js
@@ -1,7 +1,7 @@
 const { getMessage } = require('./message');
 
 describe('getMessage()', () => {
-  test('Message is Hello World!', () => {
+  test('Message is Hello World', () => {
     expect(getMessage()).toEqual('Hello World!');
   });
 });
